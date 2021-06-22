74a9d5457a4300ed922c4ef882de4152bcc7d296
Sofya <levinasof@inbox.ru>
2021-06-22 20:14:37 +0300

    add getting data from a form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { MatDatepicker } from '@angular/material/datepicker';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  dataForDocument = new FormGroup({
+    urovenObraz: new FormControl(''),
+    profil: new FormControl(''),
+    disciplina: new FormControl(''),
+    date: new FormControl(''),
+    mestoVStructure: new FormControl('')
+  });
+
+  formControl = new FormControl();
+  autoFilter: Observable<string[]> | undefined;
+
+  ngOnInit(): void {
+    this.autoFilter = this.formControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this.mat_filter(value))
+    );
+  }
+
+  private mat_filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.data.Disciplina.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+  saveData() {
+    console.log(this.dataForDocument.value)
+  }
+
   title = 'Thesis';
+  data = {
+    UrovenObraz: ["Бакалавриат", "Магистатура"],
+    Profil: ["Информационные технологии в дизайне", "Информационная поддержка жизненного цикла изделий и инфраструктуры"],
+    Disciplina: ["История",
+      "Русский язык и культура речи",
+      "Физическая культура и спорт",
+      "Иностранный язык",
+      "Дискретная математика",
+      "Информационные технологии",
+      "Математика",
+      "Графические информационные технологии",
+      "Философия",
+      "Физические основы информационно-телекоммуникационных систем",
+      "Алгоритмы и структуры данных",
+      "WEB-технологии",
+      "Объектно-ориентированное программирование"
+    ],
+    MestoVStructure: [
+      "Для дисциплин философия, история, иностранный язык, безопасность жизнедеятельности, физическая культура и спорт",
+      "Для дисциплин относящихся к базовой части образовательной программы",
+      "Для дисциплин вариативной части, определяющие направленность или специализацию ОП",
+      "Для дисциплин вариативной части, углубляющих  формирование компетенций по направленности или специализации ОП",
+      "Для факультативных дисциплин",
+    ],
 }
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,45 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+// import { MatDatepicker } from '@angular/material/datepicker';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatNativeDateModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    // MatDatepicker
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
+
   ],
-  providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  providers: [
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
