99a9f65b79708b8f8102323accd1628cc37bde97
Sofya <levinasof@inbox.ru>
2021-06-25 19:34:34 +0300

    работающая версия с flask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepicker } from '@angular/material/datepicker';
-import { Observable } from 'rxjs';
+import {identity, Observable } from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,21 +13,59 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
 
   constructor(private httpClient: HttpClient) {}
-
+  // serverTO: any;
   // sayHi(data: any) {
-  //   this.httpClient.get('http://192.168.1.84:8080/').subscribe(data => {
-  //     this.serverTO = data as JSON;
-  //     console.log(this.serverData);
-  //   })
+  //   const token = 'my JWT';
+  //   const headers = new HttpHeaders().set('authorization', 'Bearer ' + token);
+  //
+  //   this.httpClient.get('http://192.168.1.84:8080/', {headers, responseType: 'blob' as 'json'}).subscribe(data => {
+  //       (response: any) => {
+  //         let dataType = response.type;
+  //         let binaryData = [];
+  //         binaryData.push(response);
+  //         let downloadLink = document.createElement('a');
+  //         downloadLink.href = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+  //         document.body.appendChild(downloadLink);
+  //         downloadLink.click();
+  //       };
+  //     }
+  //   )
   // }
-  serverAdress = 'http://192.168.1.84:8080/';
+  serverAdress = 'http://192.168.1.221:8080/';
+  // serverAdress = 'http://192.168.1.84:8080/';
+
   sendPostRequest(data: any) {
-    return this.httpClient.post(this.serverAdress, JSON.stringify(data))
+    let config = { headers: {'Content-Type': undefined},
+      transformRequest: identity
+    }
+    return this.httpClient.post(this.serverAdress, JSON.stringify(data), {headers: new HttpHeaders({ responseType: 'text'})})
     // this.httpClient.get(this.serverAdress).subscribe(data => {
     //    data;
     //   // console.log(this.serverData);
     // })
+
   }
+  // downloadFile(route: string, filename: string = null): void{
+  //
+  //   const baseUrl = 'http://myserver/index.php/api';
+  //   const token = 'my JWT';
+  //   const headers = new HttpHeaders().set('authorization','Bearer '+token);
+  //   this.http.get(baseUrl + route,{headers, responseType: 'blob' as 'json'}).subscribe(
+  //     (response: any) =>{
+  //       let dataType = response.type;
+  //       let binaryData = [];
+  //       binaryData.push(response);
+  //       let downloadLink = document.createElement('a');
+  //       downloadLink.href = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+  //       if (filename)
+  //         downloadLink.setAttribute('download', filename);
+  //       document.body.appendChild(downloadLink);
+  //       downloadLink.click();
+  //     }
+  //   )
+  // }
+
+
   saveData() {
     // console.log(this.dataForDocument.value)
     // this.serverTO = JSON.stringify(this.dataForDocument.value);
@@ -35,7 +73,9 @@ export class AppComponent implements OnInit {
     // // console.log(this.serverTO)
     // this.sayHi(this.serverTO);
     console.log(this.dataForDocument.value)
+
     this.sendPostRequest(this.dataForDocument.value).subscribe();
+    // this.sayHi(this.dataForDocument.value)
   }
   dataForDocument = new FormGroup({
     urovenObraz: new FormControl(''),
@@ -61,6 +101,20 @@ export class AppComponent implements OnInit {
     return this.data.Disciplina.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
+  downloadFile(){
+    let link = window.document.createElement("a");
+    link.download = "test.docx";
+    link.href = "assets/test.docx";
+    link.click();
+
+    // let link = document.createElement('a');
+    // link.setAttribute('type', 'hidden');
+    // link.href = 'assets/';
+    // link.download = "test.docx";
+    // document.body.appendChild(link);
+    // link.click();
+    // link.remove();
+  }
 
   title = 'Thesis';
   data = {
@@ -85,7 +139,7 @@ export class AppComponent implements OnInit {
       "Для дисциплин относящихся к базовой части образовательной программы",
       "Для дисциплин вариативной части, определяющие направленность или специализацию ОП",
       "Для дисциплин вариативной части, углубляющих  формирование компетенций по направленности или специализации ОП",
-      "Для факультативных дисциплин",
+      "Для факультативных дисциплин"
     ],
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,20 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule} from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
-// import { MatDatepicker } from '@angular/material/datepicker';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-// import { MatNativeDateModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FormComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
